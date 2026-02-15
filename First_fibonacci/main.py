b443c46ae1f826d55d048b6112bdca54a31dbbd6
IhorMalashkov <igormalashkovjob@gmail.com>
2026-02-15 01:57:27 +0100

    Enhance caching_fibonacci with comments
    Added comments in Ukrainian to clarify caching logic and usage.

--- First_fibonacci/main.py
+++ First_fibonacci/main.py
@@ -1,17 +1,18 @@
 def caching_fibonacci():
-    cache = {}
+    cache = {} #Створення порожнього списку
     def fibonacci(n):
         if n <= 0:
             return 0
         if n == 1:
             return 1
-        if n in cache:
+        if n in cache: #Перевірка наявності значення в кеші
             return cache[n]
-        cache[n]=fibonacci(n-1) + fibonacci(n-2)
+        cache[n]=fibonacci(n-1) + fibonacci(n-2) # Якщо немає в кеші, обчислюємо рекурсивно і записуємо в кеш
         return cache[n]
     return fibonacci
 
-fib = caching_fibonacci()
+fib = caching_fibonacci()# Отримуємо функцію fibonacci
 
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
 print(fib(10))
 print(fib(15))
